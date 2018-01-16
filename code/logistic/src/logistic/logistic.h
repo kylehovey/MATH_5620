a995556bddaafed44e2145b8e70b19c9e231043d
kylehovey <khoveyc@gmail.com>
2018-01-16 12:39:31 -0700

    Used more terse syntax for lambda.

--- code/logistic/src/logistic/logistic.h
+++ code/logistic/src/logistic/logistic.h
@@ -8,9 +8,11 @@ template <typename T>
 std::function<T(T)> genLogistic(T alpha, T beta, T Po) {
   auto amp = ( alpha / Po - beta);
 
-  return std::function<T(T)>([amp, alpha, beta](T t) {
-    return alpha / (amp * exp(-alpha * t) + beta);
-  });
+  return std::function<T(T)>(
+    [=] (T t) {
+      return alpha / (amp * exp(-alpha * t) + beta);
+    }
+  );
 }
 
 #endif
